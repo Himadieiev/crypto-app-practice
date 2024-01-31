24801c4648878b6879fc23c09dba82948594b609
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-01-31 11:44:08 +0200

    added layout str

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Layout } from 'antd';
+
+import AppHeader from './components/layout/AppHeader';
+import AppSider from './components/layout/AppSider';
+import AppContent from './components/layout/AppContent';
+
 const App = () => {
-  return <h1>New App</h1>;
+  return (
+    <Layout>
+      <AppHeader />
+      <Layout>
+        <AppSider />
+        <AppContent />
+      </Layout>
+    </Layout>
+  );
 };
 
 export default App;
